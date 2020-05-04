5302cc0efc886d5c64fb2e8090b251bde74eef88
Yulia Gapunovych <45093751+YulitaGap@users.noreply.github.com>
2020-05-05 00:09:46 +0300

    Upload part 1

---
+++ src/components/maincontainer/Content.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './content.css'
+class Content extends Component {
+   render() {
+      return (
+         <div class="main_content">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+            <div id="home-img"></div>
+         </div>
+      );
+   }
+}
+export default Content
